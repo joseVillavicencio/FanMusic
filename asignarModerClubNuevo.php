926f10f415f2f4ad3f120e30a87e97fb9787142d
joseVillavicencio <thomy.va.93@gmail.com>
2016-08-30 09:35:28 -0300

    Primer prototipo
    La primera entrega de FanMusic

---
+++ asignarModerClubNuevo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+	<script type="text/javascript" src="js/jquery.js"></script>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/mainFanApp.css" rel="stylesheet">
+	<link href="css/animate.css" rel="stylesheet">	
+	<link href="css/responsive.css" rel="stylesheet">
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+	<!-- Esta parte es de nuestro codigo- -->
+	<script src="js/funcionesClub.js" type="text/javascript"></script>
+	<link href="css/jquery.dialog.css" rel="stylesheet" type="text/css"><!--NECESARIA PARA EL DIALOG/IMAGEN-->
+	<script src="js/jquery.dialog.js" type="text/javascript"></script><!--NECESARIA PARA EL DIALOG/IMAGEN-->	
+	
+</head>
+<body>
+	<section id="perfilClub">
+		<div id="designar" class="panel panel-default" align="center">
+			<div class="panel-heading">Asignar Moderador</div>
+			<div class="panel-body">Ingrese el correo del Moderador : <input type="text" id="mailMod"><br>
+			<br><br><button type="button" onclick="asignarModerador();" class="btn btn-primary">Asignar</button><br></div>
+		</div>
+	</section><!--/#explore-->
+</body>
+	<script type="text/javascript" src="js/smoothscroll.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax.js"></script>
+    <script type="text/javascript" src="js/coundown-timer.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo.js"></script>
+    <script type="text/javascript" src="js/jquery.nav.js"></script>
+</html>
