44023c3faa4002388589b15e9aca50a48a3fdcbe
Niia <shooting_star-.-@hotmail.com>
2016-09-07 13:02:43 -0300

    Anecdotas en Club
    Este codigo debiera servir para ver las anecdotas dentro del club. Uno
    de los archivos no tiene muchos cambios porque el trabajo quedó a
    medias. (muroLetras.php)

---
+++ muroAnecdotas.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<script type="text/javascript" src="js/jquery.js"></script>
+	   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet">
+		<link href="css/mainFanApp.css" rel="stylesheet">
+		<link href="css/animate.css" rel="stylesheet">	
+		<link href="css/responsive.css" rel="stylesheet">
+		<script src="js/bootstrap.js" type="text/javascript"></script>
+		<!-- Esta parte es de nuestro codigo- -->
+		<script src="js/funcionesClub.js" type="text/javascript"></script>
+		
+	</head>
+	<body>
+		<div class="row" style=" text-align:center;">
+			<div class="col-lg-1 col-sm-1">
+			</div>
+			<div id="muroClub"  class="col-lg-6 col-sm-6">
+				<script type="text/javascript">
+					muroDelClub("#muroClub");
+				</script>
+			</div>
+		</div>
+	</body>
+	 <script type="text/javascript" src="js/smoothscroll.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax.js"></script>
+    <script type="text/javascript" src="js/coundown-timer.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo.js"></script>
+    <script type="text/javascript" src="js/jquery.nav.js"></script>
+</html>
