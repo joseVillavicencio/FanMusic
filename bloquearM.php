da8c89e3dd582e8190ab4c84c5cb5e4a8b80dd25
Niia <shooting_star-.-@hotmail.com>
2016-10-04 18:00:50 -0300

    Cambios en el back-to-top
    1) aplicado a todos los archivos que se envian.
    2) cambio en el js de grupos porque había un error de tipeo

--- bloquearM.php
+++ bloquearM.php
@@ -12,6 +12,17 @@
 		<link href="css/responsive.css" rel="stylesheet">
 		<!-- Para que funcione el toolbar, ojo puede que se acople con algo..- -->
 		<script src="js/bootstrap.js" type="text/javascript"></script>
+		<style type="text/css">
+			.back-to-top {
+				cursor: pointer;
+				position: fixed;
+				bottom: 0;
+				right: 20px;
+				display:none;
+				background-color: #1B7B98;
+				color: #fff;
+			}
+		</style>
 	</head>
 	
 	<body>
@@ -93,5 +104,30 @@
 				</div>
 			</div>
 		</section>
+		<a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Back to Top" data-toggle="tooltip" data-placement="top">
+		  <span class="glyphicon glyphicon-chevron-up"></span>
+		</a>
+		<script type="text/javascript">
+			$(document).ready(function(){
+			 $(window).scroll(function () {
+					if ($(this).scrollTop() > 50) {
+						$('#back-to-top').fadeIn();
+					} else {
+						$('#back-to-top').fadeOut();
+					}
+				});
+				// scroll body to 0px on click
+				$('#back-to-top').click(function () {
+					$('#back-to-top').tooltip('hide');
+					$('body,html').animate({
+						scrollTop: 0
+					}, 800);
+					return false;
+				});
+				
+				$('#back-to-top').tooltip('show');
+
+			});
+		</script>
 	</body>
 </html>
--- gestionarFinanzasClubNuevo.php
+++ gestionarFinanzasClubNuevo.php
@@ -10,11 +10,20 @@
 	<link href="css/animate.css" rel="stylesheet">	
 	<link href="css/responsive.css" rel="stylesheet">
 	<script src="js/bootstrap.js" type="text/javascript"></script>
-	<!-- Esta parte es de nuestro codigo- -->
 	<script src="js/funcionesClub.js" type="text/javascript"></script>
 	<link href="css/jquery.dialog.css" rel="stylesheet" type="text/css"><!--NECESARIA PARA EL DIALOG/IMAGEN-->
 	<script src="js/jquery.dialog.js" type="text/javascript"></script><!--NECESARIA PARA EL DIALOG/IMAGEN-->	
-			
+	<style type="text/css">
+		.back-to-top {
+			cursor: pointer;
+			position: fixed;
+			bottom: 0;
+			right: 20px;
+			display:none;
+			background-color: #1B7B98;
+			color: #fff;
+		}
+	</style>		
 	<script>
 		$(document).ready(function(){
 				function actualizando(){ 
@@ -138,6 +147,31 @@
 			
 		</div>
 	</section><!--/#explore-->
+	<a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Back to Top" data-toggle="tooltip" data-placement="top">
+	  <span class="glyphicon glyphicon-chevron-up"></span>
+	</a>
+	<script type="text/javascript">
+		$(document).ready(function(){
+		 $(window).scroll(function () {
+				if ($(this).scrollTop() > 50) {
+					$('#back-to-top').fadeIn();
+				} else {
+					$('#back-to-top').fadeOut();
+				}
+			});
+			// scroll body to 0px on click
+			$('#back-to-top').click(function () {
+				$('#back-to-top').tooltip('hide');
+				$('body,html').animate({
+					scrollTop: 0
+				}, 800);
+				return false;
+			});
+			
+			$('#back-to-top').tooltip('show');
+
+		});
+	</script>
 </body>
 	 <footer id="footer">
         <div class="container">
