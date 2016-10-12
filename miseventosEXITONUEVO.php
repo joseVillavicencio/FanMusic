10184a82f80c85fa75f88d5b673c54970d8f7768
joseVillavicencio <thomy.va.93@gmail.com>
2016-10-12 17:20:32 -0300

    ultimos cambios hechos por jose
    borre imagenes y correccion responsive

--- miseventosEXITONUEVO.php
+++ miseventosEXITONUEVO.php
@@ -65,11 +65,11 @@
 		</div>
 	</header>
 		<section id="explore" >
-			<div class="row"  style="postion: absolute ; Height:100%" >
+			<div class="row"  style="Height:100%" >
 				<div class="watch">
 						<img class="img-responsive" src="images/watch.png" alt="">
 				</div>				
-				<div class="col-md-7 col-md-offset-3 col-sm-11" >
+				<div class="col-lg-9 col-lg-offset-3 col-md-9 col-md-offset-3 col-sm-11 col-sm-offset-1" >
 					<div id="eventos general" class="table-responsive" align="center">
 						<table class="table" >
 							<thead>
@@ -122,7 +122,7 @@
 			});
 		</script>
 		</body>
-		 <footer id="footer">
+		 <footer id="footer" style="position: relative;bottom: 0;">
 			<div class="container">
 				<div class="text-center">
 					<p> Sitio desarrollado por Dania Delgado - Tania Pizarro - Jose Villavicencio &copy;2016<br>
