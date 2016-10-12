523fce70efbb30194f87a83d1dfefb802d6d6ba4
Niia <shooting_star-.-@hotmail.com>
2016-10-12 17:43:22 -0300

    Bloquear Miembros
    Bloquear miembros en grupos y clubs ahora NO ELIMINA, sino que se hacen
    filtros a partir de la tabla bloqueados para ver si los muestra o no en
    el inicio. :)

--- php/bloquearMiembroC.php
+++ php/bloquearMiembroC.php
@@ -15,7 +15,6 @@
 				while($fila=mysqli_fetch_row($result)){ 
 					$idMiembro=$fila[0] ;
 				}
-				
 				$conexion=conectar();
 				$sql5 = "call obtenerAdminConCorreo('".$idMiembro."');"; //Verifico que no sea el Admin para no cometer errores...
 				if($registro5 =$conexion->query($sql5)){
@@ -34,26 +33,15 @@
 								if($registro3 =$conexion->query($sql3)){
 									if($registro3->num_rows >0){
 										$conexion=conectar();
-										$bloq = "call estaBloqueadoEnClub('".$idMiembro."','".$idClub."');"; //Bloqueamos al usuario en el Club
-										
+										$bloq = "call estaBloqueadoEnClub('".$idMiembro."','".$idClub."');"; //Buscamos que no se encuentre bloqueado desde antes
 										if($registro4 =$conexion->query($bloq)){
-										
 											if($registro4->num_rows >0){
-												echo 0; //ya esta bloqueado
-											}else{
-												
+												echo 0; //Se encuentra bloqueado desde antes
+											}else{ 												
 												$conexion=conectar();
-												$eliminar1 = "call eliminarMiembroGrupoAll('".$idMiembro."');"; //Lo eliminamos de todos los grupos
-												if($registro41 =$conexion->query($eliminar1)){
-												}
-												$conexion=conectar();
-												$eliminar2 = "call eliminarMiembro('".$idMiembro."','".$idClub."');"; //Lo eliminamos del club
-												if($registro4 =$conexion->query($eliminar2)){
-													$conexion=conectar();
-													$bloc = "call bloquear('".$idMiembro."','".$idClub."');";
-													if($result_bloc =$conexion->query($bloc)){
-														echo 1; //Eliminamos y bloqueamos correctamente ;)
-													}
+												$bloc = "call bloquear('".$idMiembro."','".$idClub."');";
+												if($result_bloc =$conexion->query($bloc)){
+													echo 1; //Bloqueado del Club ;)
 												}
 											}
 										}
--- php/listagrupo.php
+++ php/listagrupo.php
@@ -2,6 +2,7 @@
 	header('Content-Type: charset=utf-8');
 	include('funcionesI.php');
 	$id= $_POST["id"];
+	$FLAG=0;
 	$conexion=conectar();
 	mysqli_set_charset($conexion,"utf8");
 	
@@ -16,18 +17,54 @@
 					$nombre=$fila[1];
 					$descrip=$fila[2];
 					$club=$fila[3];
-					echo "<tr><td><img src='$imag' class='img-circle' ' border='0' width='50' height='50' ></td>"  ;
-					echo '<td><a style="color:black;" href="\FanMusic/p_gruposNuevo.php?pag='.$nombre.'" >'.$club.'-'.$nombre.'</a></td><td>'.$descrip.'</td>';
-					$conexion=conectar();
-					$sql2 = "call  gruposAdmi('".$nombre."','".$id."');";					
-					if($result2 = $conexion->query($sql2)){
-						if($result2->num_rows >0){
-							while($fila2=mysqli_fetch_row($result2)){ 
-								$id_Grupo=$fila2[0] ;
-								echo '<div><td><input  id="'.$id_Grupo.'" type="hidden"><button type="button" class="btn btn-danger btn-xs" onclick="eliminarG('.$id_Grupo.');">Eliminar</button></tr></td></div>';
+					
+					$conexion = conectar(); //Desde aquí comienza un filtrado, para no mostrar los clubs en los que está bloqueado
+					$consulta="call nombreClub('".$club."');";
+					if($resultado = $conexion->query($consulta)){
+						if($resultado->num_rows >0){
+							while($filita=mysqli_fetch_row($resultado)){
+								$id_CLUB=$filita[0];
+								$conexion = conectar();
+								$consulta2= "call estaBloqueadoEnClub('".$id."','".$id_CLUB."');";
+								if($resultado2 = $conexion->query($consulta2)){
+									if($resultado2->num_rows > 0){ //QUIERE DECIR QUE ESTÁ BLOQUEADO EN ESE CLUB
+										$FLAG=1;
+									}									
+								}
 							}
 						}
 					}
+					$conexion = conectar(); //Desde aquí comienza un filtrado, para no mostrar los grupos en los que está bloqueado
+					$consulta="call obtenerIdGrupo('".$nombre."');";
+					if($resultado = $conexion->query($consulta)){
+						if($resultado->num_rows >0){
+							while($filita=mysqli_fetch_row($resultado)){
+								$id_GRUPO=$filita[0];
+								$conexion = conectar();
+								$consulta2= "call estaBloqueadoEnGrupo('".$id."','".$id_GRUPO."');";
+								if($resultado2 = $conexion->query($consulta2)){
+									if($resultado2->num_rows > 0){ //QUIERE DECIR QUE ESTÁ BLOQUEADO EN ESE GRUPO
+										$FLAG=1;
+									}									
+								}
+							}
+						}
+					}
+					if($FLAG!=1){
+						echo "<tr><td><img src='$imag' class='img-circle' ' border='0' width='50' height='50' ></td>"  ;
+						echo '<td><a style="color:black;" href="\FanMusic/p_gruposNuevo.php?pag='.$nombre.'" >'.$club.'-'.$nombre.'</a></td><td>'.$descrip.'</td>';
+						$conexion=conectar();
+						$sql2 = "call  gruposAdmi('".$nombre."','".$id."');";					
+						if($result2 = $conexion->query($sql2)){
+							if($result2->num_rows >0){
+								while($fila2=mysqli_fetch_row($result2)){ 
+									$id_Grupo=$fila2[0] ;
+									echo '<div><td><input  id="'.$id_Grupo.'" type="hidden"><button type="button" class="btn btn-danger btn-xs" onclick="eliminarG('.$id_Grupo.');">Eliminar</button></tr></td></div>';
+								}
+							}
+						}
+					}
+					$FLAG=0;
 				}
 			}
 		}
